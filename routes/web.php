9781ac25ad9ab358c1e529bbd1e5c91c28c9036e
chungddph40543 <chungddph40543@fpt.edu.vn>
2025-02-17 21:32:30 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 // Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 // Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+// Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // Dashboard chỉ truy cập được khi đăng nhập
 Route::middleware('auth')->group(function () {
